84ebe9b9b8c6ea645d25c7631de711e77c0a2fec
yuliyang <645644717@qq.com>
2020-10-28 17:01:03 +0800

    sharedFlow和stateFlow

---
+++ app/src/main/java/com/yly/channelandflow/CoroutineUtilClassActivity.kt
@@ -0,0 +1,104 @@
+package com.yly.channelandflow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_coroutines_util_class.*
+import kotlinx.coroutines.*
+import java.lang.Exception
+import java.lang.RuntimeException
+
+/**
+ * @author    yiliyang
+ * @date      20-10-28 下午1:33
+ * @version   1.0
+ * @since     1.0
+ */
+class CoroutineUtilClassActivity : AppCompatActivity(R.layout.activity_coroutines_util_class) {
+
+    val mainScope = MainScope()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        test.setOnClickListener {
+//            testCompletableDeffer()
+
+//            testLazyJob()
+
+            testCancel()
+
+//            testCompleteable()
+        }
+    }
+
+    private fun testCompleteable() {
+        val completableJob = SupervisorJob()
+        with(CoroutineScope(completableJob)) {
+            launch {
+                while (this.isActive) {
+                    println("ticker")
+                    delay(1000)
+                }
+            }
+        }
+        GlobalScope.launch {
+            delay(3000)
+            completableJob.complete()
+        }
+    }
+
+    private fun testCancel() {
+        GlobalScope.launch {
+            val job = launch {
+                try {
+                    repeat(100) {
+                        delay(1000)
+                        println("ticker")
+                    }
+                } catch (e: CancellationException) {
+                    println("catch ${e.message}")
+                }
+            }
+
+            job.invokeOnCompletion {
+                println("xxx")
+            }
+            launch {
+                delay(3000)
+                job.cancel()
+//                repeat(100) {
+//                    println("canceled")
+//                    delay(1000)
+//                }
+            }
+        }
+    }
+
+    private fun testLazyJob() {
+        val job = GlobalScope.launch(start = CoroutineStart.LAZY) {
+            println("lazy")
+        }
+
+        GlobalScope.launch {
+            job.start()
+//                job.join()
+        }
+    }
+
+    private fun testCompletableDeffer() {
+        val completableDeferred = CompletableDeferred<String>()
+        val handler = CoroutineExceptionHandler { _, throwable ->
+            println(throwable.message)
+        }
+        GlobalScope.launch(handler) {
+            launch {
+                val result = completableDeferred.await()
+                println("await $result")
+            }
+            launch {
+                delay(2000)
+//                    completableDeferred.complete("complete")
+                completableDeferred.completeExceptionally(RuntimeException("error"))
+            }
+        }
+    }
+}
